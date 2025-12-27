5925ca6d74480c94e30a3a5c1545bcd7d9896a09
Egor Rudakov <rudakovegor409@gmail.com>
2025-12-27 14:27:13 +0500

    fixed

--- src/main/java/com/telegram/musicplayer/controller/MiniAppController.java
+++ src/main/java/com/telegram/musicplayer/controller/MiniAppController.java
@@ -9,6 +9,6 @@ public class MiniAppController {
     @GetMapping("/miniapp")
     public String miniApp() {
         // forward вместо redirect
-        return "https://telegram-music-player-uh5o.onrender.com/miniapp";
+        return "forward:/miniapp/index.html";
     }
 }
