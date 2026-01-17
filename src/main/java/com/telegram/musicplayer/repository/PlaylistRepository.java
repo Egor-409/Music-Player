cc9d28ed6d969e5a9e11629a540897c3c2782d26
Egor Rudakov <rudakovegor409@gmail.com>
2026-01-17 12:50:33 +0500

    update to SPA-index.html

--- src/main/java/com/telegram/musicplayer/repository/PlaylistRepository.java
+++
@@ -1,7 +0,0 @@
-package com.telegram.musicplayer.repository;
-
-import com.telegram.musicplayer.model.Playlist;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PlaylistRepository extends JpaRepository<Playlist, Long> {
-}
